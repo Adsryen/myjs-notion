a77e8d075bde6cd7a4c36503906f0f83a8f8ebf0
Anderson-Ryen <prl1594959462@gmail.com>
2022-02-28 00:30:00 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: 'The-course-of-the-postgraduate-entrance-examination-d753c7665e524f54a7beec1d2d8cdb97',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'The course of the postgraduate entrance examination',
+  domain: 'https://www.notion.so/prlrr/The-course-of-the-postgraduate-entrance-examination-d753c7665e524f54a7beec1d2d8cdb97',
+  author: 'Anderson Ryen',
 
   // open graph metadata (optional)
   description: 'Example site description',
@@ -17,9 +17,9 @@ module.exports = {
   socialImageSubtitle: 'Hello World! 👋',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: '',
+  github: 'Adsryen',
+  linkedin: '',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
